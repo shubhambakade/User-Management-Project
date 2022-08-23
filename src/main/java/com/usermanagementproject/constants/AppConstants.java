b0ae0d689bb6fd943369caf4f7ab410729e34184
shubhambakade <ShubhamBakade25@gmail.com>
2022-08-23 15:39:00 +0530

    UMP-10 :create app constants package and app constants class

---
+++ src/main/java/com/usermanagementproject/constants/AppConstants.java
@@ -0,0 +1,52 @@
+package com.usermanagementproject.constants;
+
+public class AppConstants {
+
+private AppConstants() {
+		
+	}
+	
+	public static final String REST_CONTROLLER_PACKAGE = "in.um.rest";
+
+	public static final String UNLOCK_SUCCESS = "unlockSucc";
+
+	public static final String UNLOCK_FAIL = "unlockFail";
+
+	public static final String UNIQUE = "Unique";
+
+	public static final String DUPLICATE = "Duplicate";
+	
+	public static final String SUCCESS = "Success";
+
+	public static final String FAILED = "Failed";
+
+	public static final String LOCKED = "Locked";
+
+	public static final String UN_LOCKED = "Unlocked";
+
+	public static final String INVALID_CREDENTIALS = "invalidCredentials";
+
+	public static final String ACCOUNT_LOCKED = "accountLocked";
+
+	public static final String REG_EMAIL_SUB = "regEmailSub";
+
+	public static final String FORGOT_PWD_EMAIL_SUB = "forgotPwdEmailSub";
+
+	public static final String FORGOT_PWD_SUCC = "forgotPwdSucc";
+
+	public static final String FORGOT_PWD_FAILED = "forgotPwdFailed";
+
+	public static final String REG_MAIL_BODY_FILE = "regmailBodyFile";
+
+	public static final String FORGOT_PWD_MAIL_BODY_FILE = "forgotPwdmailBodyFile";
+	
+	public static final String FIRSTNAME = "{FIRSTNAME}";
+	
+	public static final String LASTNAME = "{LASTNAME}";
+	
+	public static final String PWD = "{PWD}";
+	
+	public static final String TEMP_PWD = "{TEMP_PWD}";
+	
+	public static final String EMAIL = "{EMAIL}";
+}
